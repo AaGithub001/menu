246b77166dda232b2bd391d7a70e7862f48746f2
AaGithub001 <aasigning001@gmail.com>
2023-04-06 12:34:32 +0545

    app menu changed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const filterItems = (category) => {
 return ( <main>
     <section className='menu section'>
       <div className='title'>
-        <h2>our menu</h2>
+        <h2>our menus</h2>
         <div className='underline'></div>
       </div>
       <Categories categories={categories} filterItems={filterItems}/>
